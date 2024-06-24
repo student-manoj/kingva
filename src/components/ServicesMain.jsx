74b67ce1c0b81cfacd79a93e17bde7b0f20f6fe9
Student Manoj <manoj02biswas@gmail.con>
2024-06-24 18:30:38 +0530

    first commit

---
+++ src/components/ServicesMain.jsx
@@ -0,0 +1,18 @@
+import HeroSection from "./HeroSection";
+import PlansSection from "./PlansSection";
+import ServicesSectionTow from "./ServicesSectionTow";
+import TeamSection from "./Teamsection";
+import Teamsection from "./Teamsection";
+
+const ServicesMain = () => {
+  return (
+    <>
+      <HeroSection title="Services" pageName="Services" />
+      <ServicesSectionTow />
+      <TeamSection />
+      <PlansSection />
+    </>
+  );
+};
+
+export default ServicesMain;
---
+++ src/utils/CustmerReviewData.js
@@ -0,0 +1,31 @@
+import ReviewImg from "../../public/CustmerReviewImg.png";
+import ReviewImg1 from "../../public/CustmerReviewImg1.png";
+import ReviewImg2 from "../../public/CustmerReviewImg2.png";
+import ReviewImg3 from "../../public/CustmerReviewImg3.png";
+
+export const CustmerReviewData = [
+  {
+    id: 1,
+    title: "Happy Customers",
+    img: ReviewImg,
+    reviewpoint: "555",
+  },
+  {
+    id: 2,
+    title: "Projects Finished ",
+    img: ReviewImg1,
+    reviewpoint: "958",
+  },
+  {
+    id: 3,
+    title: "Award Winning",
+    img: ReviewImg2,
+    reviewpoint: "254",
+  },
+  {
+    id: 4,
+    title: "Years experience",
+    img: ReviewImg3,
+    reviewpoint: "548",
+  },
+];
